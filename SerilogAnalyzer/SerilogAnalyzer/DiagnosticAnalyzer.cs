57652ca3510c950952ed84ff5f19c22c5af7da00
Suchiman <robinsue@live.de>
2016-05-28 19:43:02 +0200

    improve detection

--- SerilogAnalyzer/SerilogAnalyzer/DiagnosticAnalyzer.cs
+++ SerilogAnalyzer/SerilogAnalyzer/DiagnosticAnalyzer.cs
@@ -28,11 +28,8 @@ namespace SerilogAnalyzer
 
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
             context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.InvocationExpression);
         }
-
 
         private static void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
         {
@@ -40,10 +37,10 @@ namespace SerilogAnalyzer
             var info = context.SemanticModel.GetSymbolInfo(invocation);
             var method = info.Symbol as IMethodSymbol;
 
-            // is it a method on the ILogger interface?
-            var ilogger = context.SemanticModel.Compilation.GetTypeByMetadataName("Serilog.ILogger");
-            var log = context.SemanticModel.Compilation.GetTypeByMetadataName("Serilog.Log");
-            if (method?.ContainingType != ilogger && method?.ContainingType != log)
+            // is it a serilog logging method?
+            var attributes = method.GetAttributes();
+            var messageTemplateAttribute = context.SemanticModel.Compilation.GetTypeByMetadataName("Serilog.Core.MessageTemplateFormatMethodAttribute");
+            if (!attributes.Any(x => x.AttributeClass == messageTemplateAttribute))
             {
                 return;
             }
@@ -51,6 +48,12 @@ namespace SerilogAnalyzer
             // is this an overload where the exception argument is used?
             var exception = context.SemanticModel.Compilation.GetTypeByMetadataName("System.Exception");
             if (method.Parameters.First().Type == exception)
+            {
+                return;
+            }
+
+            // is there an overload with the exception argument?
+            if (!method.ContainingType.GetMembers().OfType<IMethodSymbol>().Any(x => x.Name == method.Name && x.Parameters.FirstOrDefault()?.Type == exception))
             {
                 return;
             }
@@ -64,15 +67,6 @@ namespace SerilogAnalyzer
                     context.ReportDiagnostic(Diagnostic.Create(Rule, argument.GetLocation(), argument.Expression.ToFullString()));
                 }
             }
-            //var attributes = method.GetAttributes();
-            // Find just those named type symbols with names containing lowercase letters.
-            //if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
-            //{
-            //    // For all such symbols, produce a diagnostic.
-            //    var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
-            //    context.ReportDiagnostic(diagnostic);
-            //}
         }
 
         private static bool IsException(ITypeSymbol exception, ITypeSymbol type)
